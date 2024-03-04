6285e2877b083b639367db4e573d1e26a968f07d
funny_233 <83470506+funny233-github@users.noreply.github.com>
2024-03-04 15:36:05 +0800

    init

---
+++ example.c
@@ -0,0 +1,56 @@
+#include "tinyunit.h"
+
+static int a, b;
+
+void tu_setup()
+{
+    a = 66;
+    b = 5;
+}
+
+void tu_teardown()
+{
+    return;
+}
+
+TU_TEST(test_tu_setup)
+{
+    TU_ASSERT_INT_EQ(a, 66);
+    TU_ASSERT_INT_EQ(b, 5);
+}
+
+TU_TEST(test_add)
+{
+    TU_ASSERT(1 + 2 == 4, "test add() fail"); // fail
+    TU_ASSERT(1 + 3 == 4, "test add() fail"); // pass
+
+    TU_ASSERT_INT_EQ(1 + 2, 2); // fail
+    TU_ASSERT_INT_EQ(1 + 2, 3); // pass
+
+    TU_ASSERT_DOUBLE_EQ(0.1 + 0.2, 0.4); // fail
+    TU_ASSERT_DOUBLE_EQ(0.1 + 0.2, 0.3); // pass
+
+    TU_ASSERT_STR_EQ("hello", "hellow"); // fail
+    TU_ASSERT_STR_EQ("hello", NULL); // fail
+    TU_ASSERT_STR_EQ(NULL, "hello"); // fail
+    TU_ASSERT_STR_EQ(NULL, NULL); // pass
+    TU_ASSERT_STR_EQ("hello", "hello"); // pass
+}
+
+TU_TEST(test_sub)
+{
+    TU_ASSERT(2 - 1 == 1, "test sub() fail"); // pass
+}
+
+TU_TEST(test_all)
+{
+    TU_RUN(test_tu_setup);
+    TU_RUN(test_add);
+    TU_RUN(test_sub);
+}
+
+int main()
+{
+    test_all();
+    return TU_EXIT_CODE;
+}
---
+++ tinyunit.h
@@ -0,0 +1,64 @@
+#ifndef __TINYUNIT_H__
+#define __TINYUNIT_H__
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+static int tu_exit_code = 0;
+#define TU_EXIT_CODE tu_exit_code
+#define TU_EXIT_FAIL \
+    TU_EXIT_CODE = -1
+
+#define TU_ASSERT(test, message)                                             \
+    do {                                                                     \
+        if (!(test)) {                                                       \
+            printf("%s faild--l:%d--%s\n", __FUNCTION__, __LINE__, message); \
+            TU_EXIT_FAIL;                                                    \
+        }                                                                    \
+    } while (0)
+
+#define TU_ASSERT_INT_EQ(result, expected)                                  \
+    do {                                                                    \
+        if (!((result) == (expected))) {                                    \
+            printf("%s faild--l:%d--result:%d,expected:%d\n", __FUNCTION__, \
+                __LINE__, result, expected);                                \
+            TU_EXIT_FAIL;                                                   \
+        }                                                                   \
+    } while (0)
+
+#define TU_ASSERT_DOUBLE_EQ(result, expected)                               \
+    do {                                                                    \
+        if (fabs((result) - (expected)) > 1E-12) {                          \
+            printf("%s faild--l:%d--result:%f,expected:%f\n", __FUNCTION__, \
+                __LINE__, result, expected);                                \
+            TU_EXIT_FAIL;                                                   \
+        }                                                                   \
+    } while (0)
+
+#define STR_NULL "__STRING_NULL__"
+#define TU_ASSERT_STR_EQ(result, expected)                                  \
+    do {                                                                    \
+        char* result_p = result;                                            \
+        char* expected_p = expected;                                        \
+        if (!result_p)                                                      \
+            result_p = STR_NULL;                                            \
+        if (!expected_p)                                                    \
+            expected_p = STR_NULL;                                          \
+        if (!(strcmp(result_p, expected_p) == 0)) {                         \
+            printf("%s faild--l:%d--result:%s,expected:%s\n", __FUNCTION__, \
+                __LINE__, result_p, expected_p);                            \
+            TU_EXIT_FAIL;                                                   \
+        }                                                                   \
+    } while (0)
+
+#define TU_RUN(test)     \
+    do {                 \
+        tu_setup();    \
+        test();          \
+        tu_teardown(); \
+    } while (0)
+
+#define TU_TEST(test) static void test()
+
+#endif
